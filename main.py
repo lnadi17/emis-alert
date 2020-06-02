2e30736b217609a81093d6234b9935911b0ce3cc
Luka Nadiradze <19193250+lnadi17@users.noreply.github.com>
2020-06-02 15:39:16 +0400

    Parse table contents

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os.path
 from sys import argv
 from os import system
 from time import sleep
+from bs4 import BeautifulSoup as bs
 
 def main():
     url = 'https://emis.freeuni.edu.ge/index.php?mode=info&sub=nishnebi&lang=ka'
@@ -22,17 +23,36 @@ def main():
             with open('old', 'w') as f:
                 f.write(r.text)
             wait()
+        
+        soup = bs(r.text, 'lxml')
+        for semester_block in soup.findAll("div", {"class": "sem_block"}):
+            title = semester_block.find_next("div", {"class": "block_title"})
+            print(title.string[0] + ' სემესტრი')
+            table_body = semester_block.find_next("tbody")
+            rows = table_body.find_all("tr", recursive=False)
+            for row in rows:
+                print('data-lid:', row['data-lid'])
+                cols = row.find_all('td', recursive=False)
+                print('name:', ' '.join(cols[0].string.split()[1:]))
+                print('score:', cols[3].string, '(' + cols[4].string + ')')
+        
+        return
 
         while True:
             print('getting new sample')
             r = s.get(url, headers=headers, cookies=cookies)
             print('response:', r)
+            print(r.text)
 
             with open('new', 'w') as f:
                 f.write(r.text)
 
             if changed():
                 print('website content changed!')
+                # create soup
+                soup = bs(r.text)
+                for table in bs.find_all('table'):
+                    print(table)
                 break
 
             system('mv new old')
@@ -42,7 +62,7 @@ def changed():
     return not filecmp.cmp('old', 'new', shallow=False) 
 
 def wait():
-    sleep(10)
+    sleep(5)
 
 if __name__ == '__main__':
     main()
